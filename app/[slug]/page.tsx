5692f4f967437fe5274f71063c254cec150f5e7d
OVIS <ovi@ovswebsites.com>
2023-12-14 06:09:29 +0200

    previews for individual post route

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,8 +1,12 @@
 import { SanityDocument } from "@sanity/client";
 import Post from "@/app/_components/Post";
 import { postPathsQuery, postQuery } from "@/sanity/lib/queries";
-import { sanityFetch } from "@/sanity/lib/sanityFetch";
+import { sanityFetch, token } from "@/sanity/lib/sanityFetch";
 import { client } from "@/sanity/lib/client";
+
+import { draftMode } from "next/headers";
+import PreviewProvider from "@/app/_components/PreviewProvider";
+import PreviewPost from "@/app/_components/PreviewPost";
 
 // Prepare Next.js to know which routes already exist
 export async function generateStaticParams() {
@@ -14,6 +18,15 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }: { params: any }) {
   const post = await sanityFetch<SanityDocument>({ query: postQuery, params });
+  const isDraftMode = draftMode().isEnabled;
+
+  if (isDraftMode && token) {
+    return (
+      <PreviewProvider token={token}>
+        <PreviewPost post={post} />
+      </PreviewProvider>
+    );
+  }
 
   return <Post post={post} />;
 }
